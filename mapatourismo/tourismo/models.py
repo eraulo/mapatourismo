4990bbf8a3c5edc159bdf21208185d93c4f252db
eraulo <dedeus_mariano@yahoo.com>
2017-04-26 17:00:39 +0900

    added fields in models

--- mapatourismo/tourismo/models.py
+++ mapatourismo/tourismo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from djgeojson.fields import PolygonField
 
 # Create your models here.
+
+class Tourismo(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    picture = models.ImageField()
+    geom = PolygonField()
+
+    def __unicode__(self):
+        return self.title
+
+    @property
+    def picture_url(self):
+        return self.picture.url
