b188c68b8303fb253373b6c2fc7d14bba3a2392d
eraulo <dedeus_mariano@yahoo.com>
2017-04-26 17:02:27 +0900

    added static and data geojson

--- mapatourismo/tourismo/urls.py
+++ mapatourismo/tourismo/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from tourismo import views
+from django.conf.urls.static import static
+from djgeojson.views import GeoJSONLayerView
+from .models import Tourismo
+from django.conf import settings
 
 urlpatterns=[
-        url(r'^$', views.Tourismo.as_view(), name="tourismo")
-]
+        url(r'^$', views.Tourismo.as_view(), name="tourismo"),
+        url(r'^data.geojson$', GeoJSONLayerView.as_view(model=Tourismo, properties=('title', 'description', 'picture_url')), name='data')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
