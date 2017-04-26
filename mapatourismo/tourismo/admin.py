9f15e74e054b99eac2eb164b4f5e31529fc8ad61
eraulo <dedeus_mariano@yahoo.com>
2017-04-26 16:59:53 +0900

    added fields in models

--- mapatourismo/tourismo/admin.py
+++ mapatourismo/tourismo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
+from . import models as tourismo_models
 
 # Register your models here.
+admin.site.register(tourismo_models.Tourismo, LeafletGeoAdmin)
